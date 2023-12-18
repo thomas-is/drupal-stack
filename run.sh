40977ae1049b5cbc78f447fe9d97a5e19ab07871
thomas-is <thomas@0l.fi>
2023-12-18 23:27:08 +0100

    🐚 create volumes dirs at startup

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ base=$( realpath $( dirname $0 ) )
 
 docker build -t 0lfi/drupal $base/docker || exit 1
 
+mkdir -p $base/volumes/drupal
+mkdir -p $base/volumes/mysql
+
 cd $base
 docker network create --attachable -d overlay drupal
 docker stack deploy -c drupal.yml drupal
