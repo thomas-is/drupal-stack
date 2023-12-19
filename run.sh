bf3a2b5c4cde2ebd00b14dc8ce4b8531eba85565
thomas-is <thomas@0l.fi>
2023-12-19 08:19:55 +0100

    🐚 use absolute path, suppress error on docker network create

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ docker build -t 0lfi/drupal $base/docker || exit 1
 mkdir -p $base/volumes/drupal
 mkdir -p $base/volumes/mysql
 
-cd $base
-docker network create --attachable -d overlay drupal
-docker stack deploy -c drupal.yml drupal
+docker network create --attachable -d overlay drupal 2> /dev/null
+
+docker stack deploy -c $base/drupal.yml drupal
